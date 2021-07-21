e47feb54ba7ba1b2e06ec23a091a146c642d2e55
J-emmerich <emmerichjoao@protonmail.com>
2021-07-21 12:59:44 +0200

    insert jest globals to test user-data-controller

--- __tests__/user-data-controller.js
+++ __tests__/user-data-controller.js
@@ -1,11 +1,12 @@
-const createUser = require("../app/controller/user-data-controller");
+const { describe, expect, test } = require("@jest/globals");
+const createUser = require("../app/controller/user-data-access");
 
 describe("Crud operations", () => {
   test("Returns an user", async () => {
     const db = {
       create(user) {
         return `${user.name} was created:::: ${user}`;
-      }
+      },
     };
 
     const user = await createUser(db);
